4d97ce5df03d319934a8a88bb7d7f0e7e525e0b5
William Mork <w.j.mork@gmail.com>
2019-03-07 17:27:25 -0600

    created boilerplate functions for symbol table.

--- src/symboltable/Symbol.java
+++ src/symboltable/Symbol.java
@@ -5,6 +5,38 @@ import java.util.ArrayList;
 public class Symbol {
     String lexeme;
     String type;
-    // int index, terminal;
-    ArrayList<Symbol> args;
+    String kind;
+    int startIndex;
+    int stopIndex;
+    ArrayList<Symbol> arguments;
+
+    // Program symbol constructor
+    public Symbol(String l, String k) {
+        lexeme = l;
+        kind = k;
+    }
+
+    // Variable symbol constructor
+    public Symbol(String l, String t, String k) {
+        lexeme = l;
+        type = t;
+        kind = k;
+    }
+
+    // Array symbol constructor
+    public Symbol(String l, String t, String k, int start, int stop) {
+        lexeme = l;
+        type = t;
+        kind = k;
+        startIndex = start;
+        stopIndex = stop;
+    }
+
+    // Function symbol constructor
+    public Symbol(String l, String t, String k, ArrayList args) {
+        lexeme = l;
+        type = t;
+        kind = k;
+        arguments = args;
+    }
 }
--- src/symboltable/SymbolTable.java
+++ src/symboltable/SymbolTable.java
@@ -6,4 +6,41 @@ import java.util.HashMap;
 
 public class SymbolTable {
     protected HashMap<String, Symbol> symbolTable;
+
+    public SymbolTable() {
+        symbolTable = new HashMap<>();
+    }
+
+    public boolean addProgram(){
+        return false;
+    }
+
+    public boolean addVariable(){
+        return false;
+    }
+
+    public boolean addArray(){
+        return false;
+    }
+
+    public boolean addFunction(){
+        return false;
+    }
+
+    public boolean isProgram(){
+        return false;
+    }
+
+    public boolean isVariable(){
+        return false;
+    }
+
+    public boolean isArray(){
+        return false;
+    }
+
+    public boolean isFunction(){
+        return false;
+    }
+
 }
